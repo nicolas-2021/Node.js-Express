f3e2c64959867b6cd49c2ce002b7395c297d86ca
Amuedo Nicolas <nicolasmartinamuedo1@gmail.com>
2021-09-26 18:35:30 -0300

    Add serve http file

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 console.log("Hello World");
 var express = require('express');
 var app = express();
-app.get("/",function(req,res){
+/*app.get("/",function(req,res){
 	res.send("Hello Express");
+});*/
+var rutaAbsoluta = __dirname+"//views/index.html"; 
+app.get("/",function(req,res){
+	res.sendFile(rutaAbsoluta);
 });
 
 
