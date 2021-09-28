c8b55c3b4518b4e9ed5dcd211e6cb8d4d195a0e0
Amuedo Nicolas <nicolasmartinamuedo1@gmail.com>
2021-09-28 20:46:38 -0300

    Serve an object JSON

--- myApp.js
+++ myApp.js
@@ -4,12 +4,15 @@ var app = express();
 /*app.get("/",function(req,res){
 	res.send("Hello Express");
 });*/
-var rutaAbsoluta = __dirname+"//views/index.html"; 
+/*var rutaAbsoluta = __dirname+"//views/index.html"; 
 app.get("/",function(req,res){
 	res.sendFile(rutaAbsoluta);
 });
 var absoluteRoot = __dirname+"/public";
-app.use("/public",express.static(absoluteRoot));
+app.use("/public",express.static(absoluteRoot));*/
+app.get("/json",function(req,res){
+	res.json({"message":"Hello json"});
+});
 
 
 
