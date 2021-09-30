a3789c66c097ecb84d16bd2f05ceec3844153433
Amuedo Nicolas <nicolasmartinamuedo1@gmail.com>
2021-09-30 19:23:55 -0300

    Add .env file and a secret key

--- myApp.js
+++ myApp.js
@@ -11,7 +11,11 @@ app.get("/",function(req,res){
 var absoluteRoot = __dirname+"/public";
 app.use("/public",express.static(absoluteRoot));*/
 app.get("/json",function(req,res){
+	if(process.env.MESSAGE_STYLE==="uppercase"){
+	res.json({"message":"hello json".toUpperCase()});
+	}else{
 	res.json({"message":"Hello json"});
+	}
 });
 
 
