4ecd99f4b0109336b288d50ffb843f09f9ae5c2d
Amuedo Nicolas <nicolasmartinamuedo1@gmail.com>
2021-09-30 22:30:04 -0300

    Add middleware

--- myApp.js
+++ myApp.js
@@ -10,13 +10,17 @@ app.get("/",function(req,res){
 });
 var absoluteRoot = __dirname+"/public";
 app.use("/public",express.static(absoluteRoot));*/
-app.get("/json",function(req,res){
+/*app.get("/json",function(req,res){
 	if(process.env.MESSAGE_STYLE==="uppercase"){
 	res.json({"message":"hello json".toUpperCase()});
 	}else{
 	res.json({"message":"Hello json"});
 	}
-});
+});*/
+app.use(function(req,res,next){
+	console.log(req.method req.path - req.ip);
+	next();
+})
 
 
 
