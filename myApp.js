aa30dadd82a10f6eec82905ff1bf3db441f54134
Amuedo Nicolas <nicolasmartinamuedo1@gmail.com>
2021-10-16 20:28:23 -0300

    GET route input from the Client

--- myApp.js
+++ myApp.js
@@ -21,14 +21,17 @@ app.use("/public",express.static(absoluteRoot));*/
 	console.log(req.method req.path - req.ip);
 	next();
 });*/
-app.get("/now",function(req,res,next){
+/*app.get("/now",function(req,res,next){
 req.time = new Date().toString();
 next();
 },
 function(req,res){
 res.json({time : req.time});
 }
-);
+);*/
+app.get("/:word/echo",function(req,res){
+  res.json({echo : req.params.word})
+});
 
 
 
